dd535eba76c89f4b2601b72126fe8686ad150544
ulxsth <ulxsth@gmail.com>
2025-09-18 00:39:39 +0900

    chore: ボードを描画

--- app/reversiPlayer/main.go
+++ app/reversiPlayer/main.go
@@ -1,28 +1,57 @@
 package main
 
 import (
+	"image/color"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/vector"
 )
 
 type Game struct{}
+
+const (
+	BoardSize   = 8
+	CellSize    = 60
+	BoardOffset = 50
+)
+
+// colorToRGB は0-255の色値をRGBに変換する
+func colorToRGB(c uint8) color.RGBA {
+	return color.RGBA{R: c, G: c, B: c, A: 255}
+}
 
 func (g *Game) Update() error {
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "Hello, World!")
+	// 画面を薄いグレーでクリア
+	screen.Fill(color.RGBA{240, 240, 240, 255})
+
+	// ボードのグリッドを描画
+	for i := 0; i <= BoardSize; i++ {
+		x := float32(BoardOffset + i*CellSize)
+		y1 := float32(BoardOffset)
+		y2 := float32(BoardOffset + BoardSize*CellSize)
+
+		// 縦線
+		vector.StrokeLine(screen, x, y1, x, y2, 2, color.Black, false)
+
+		// 横線
+		y := float32(BoardOffset + i*CellSize)
+		x1 := float32(BoardOffset)
+		x2 := float32(BoardOffset + BoardSize*CellSize)
+		vector.StrokeLine(screen, x1, y, x2, y, 2, color.Black, false)
+	}
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 320, 240
+	return 800, 600
 }
 
 func main() {
-	ebiten.SetWindowSize(640, 480)
+	ebiten.SetWindowSize(800, 600)
 	ebiten.SetWindowTitle("Hello, World!")
 	if err := ebiten.RunGame(&Game{}); err != nil {
 		log.Fatal(err)
